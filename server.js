fa833660d6fdfa89326f7ee20b0dd4a41fee9a05
Cristobal Barberis <cristobal@barberis.com>
2022-08-15 17:13:06 -0400

    use port enviroment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
@@ -10,23 +11,9 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
-
-function filterByQuery(query, animalsArray) {
-  let filteredResults = animalsArray;
-  if (query.diet) {
-    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-  }
-  if (query.species) {
-    filteredResults = filteredResults.filter(animal => animal.species === query.species);
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(animal => animal.name === query.name);
-  }
-  return filteredResults;
-}
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
